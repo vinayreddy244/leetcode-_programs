7d0f8c2b8d161a2499ba35122952684b3915d528
vinayreddy244 <83955648+vinayreddy244@users.noreply.github.com>
2023-02-02 11:37:35 +0530

    Added solution - LeetHub

---
+++ Rearrange the array - GFG/rearrange-the-array.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().rearrangeArray(arr, n);
+            for (int i = 0; i < n; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solution {
+    void rearrangeArray(int[] arr, int n) {
+        Arrays.sort(arr);
+        int temp[]=arr.clone();
+        int low=0;
+        int high=n-1;
+        boolean flag=false;
+        for(int i=0;i<n;i++)
+        {
+            if(flag)
+            {
+                arr[i]=temp[high--];
+            }
+            else
+            {
+                arr[i]=temp[low++];
+            }
+            flag=!flag;
+        }
+        // code here
+    }
+}
